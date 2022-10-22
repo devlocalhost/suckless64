ef6bfc45115203bd8cc4e0cb7f1af96c32f358c1
dev64 (devlocalhost) <lazarecobani@gmail.com>
2022-10-22 23:31:43 +0300

    Revert "bruh"
    This reverts commit bae96f7cd9fbbab6a2dd7fcc8fb981a8aee92ff4.

--- dwm/config.def.h
+++ dwm/config.def.h
@@ -10,7 +10,7 @@ static const char dmenufont[]       = "JetBrainsMono-Regular:size=10";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#111111"; /* #eeeeee */
+static const char col_gray4[]       = "#000000"; /* #eeeeee */
 static const char col_cyan[]        = "#7289da";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
